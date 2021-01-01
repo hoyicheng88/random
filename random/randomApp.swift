5dabc200827ce36e2990e13f71f7fa33603c11ec
c8610290927 <c8610290927@gmail.com>
2021-01-01 14:35:20 +0800

    Initial Commit

---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by User20 on 2021/1/1.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
